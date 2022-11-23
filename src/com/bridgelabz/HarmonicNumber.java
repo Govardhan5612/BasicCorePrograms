dd58f5c94af2707d68923db77e6d292d4997c392
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-11-23 21:30:04 +0530

    Find the Nth term of harmonic value

---
+++ src/com/bridgelabz/HarmonicNumber.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * @GovardhanReddy
+ */
+public class HarmonicNumber {
+    /**
+     * find the Nth term of harmonic number
+     * @param args
+     */
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the harmonic value ");
+        float harmonicValue= sc.nextFloat();
+        float harmonicNumber=0.0f;
+        System.out.print("Harmonic numbers : ");
+        for(float i=1;i<=harmonicValue;i++){
+            float temp=1/i;
+            harmonicNumber=temp+harmonicNumber;
+            System.out.print(harmonicNumber +" ,");
+        }
+        System.out.println();
+        System.out.println("Nth Harmonic number is "+harmonicNumber);
+    }
+}
